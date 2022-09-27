408deb9edff76e2e94028e67abe658b2e61b254f
Trey Glover <trey.glover777@gmail.com>
2022-09-27 18:16:55 -0500

    minor styling changes

--- capstone/vue/src/components/TournamentCard.vue
+++ capstone/vue/src/components/TournamentCard.vue
@@ -5,7 +5,7 @@
                   {{tournament.tournamentName}}
               <!-- </router-link> -->
           </h2>
-          <img v-if='tournament.imgUrl' v-bind:src="tournament.imgUrl"/>
+          <img class='tournament-icon' v-if='tournament.imgUrl' v-bind:src="tournament.imgUrl"/>
           <ul class='tournament-minor-details'>
               <li class='host-details'>
                   <!-- <router-link to host page> -->
@@ -18,8 +18,8 @@
           <div class="tournament-status-container">
               <h1 class = "tournament-status-display">{{tournament.status}}</h1>
               <p class = "tournament-is-closed-text" v-if="tournament.status == 'closed'">Not accepting sign-ups</p>
-              <p class = "tournament-is-open-text" v-if="tournament.status == 'open'">Accepting sign-up</p>
-              <p class = "tournament-is-neither-open-nor-closed-something-is-wrong-text" v-if="tournament.status == null">Something is wrong</p>
+              <p class = "tournament-is-open-text" v-if="tournament.status == 'open'">Accepting sign-ups</p>
+              <p class = "tournament-is-neither-open-nor-closed-something-is-wrong-text" v-if="tournament.status != 'open' && tournament.status != 'closed'">Something is wrong</p>
           </div>
   </div>
 </template>
