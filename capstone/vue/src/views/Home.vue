3f3aad8c43061de7a29a1463cabccc4122ad6aba
Trey Glover <trey.glover777@gmail.com>
2022-09-27 16:47:43 -0500

    added tournament list to home page

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <tournament-list/>
     <p>You must be NOT authenticated to see this</p>
     <home-description/>
   </div>
@@ -8,8 +9,12 @@
 
 <script>
 import HomeDescription from '../components/HomeDescription.vue';
+import TournamentList from '..components/TournamentList.vue';
 export default {
-  components: { HomeDescription },
+  components: { 
+    HomeDescription,
+    TournamentList
+   },
   name: "home"
 };
 </script>
