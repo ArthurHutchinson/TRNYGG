1984a0177d13e61a8c3520fb0aa61240b79151ed
Arthur Hutchinson <arthur.y.hutchinson@gmail.com>
2022-09-26 20:52:54 -0400

    Created new folder 'concepts' to view concepts. Changed created.sh so it includes Tyler's tournament.sql when we need to clear out all data within the current database.

---
+++ capstone/java/database/selectquery.sql
@@ -0,0 +1,16 @@
+-- How to use this:
+-- Have your cursor select the line you wish to run.
+-- Then press F5 or click the Execute Button (should look like a play button.)
+
+--  For example, you hover your cursor over "SELECT * FROM users;""
+--  Select it by holding down left-mouse button and dragging it.
+--  Then press F5 or click the execute button which will execute: "SELECT * FROM users;"
+--  It will display everything on the users table.
+
+SELECT * FROM users;
+
+SELECT * FROM tournaments;
+
+SELECT * FROM tournament_user;
+
+SELECT * FROM matches;
