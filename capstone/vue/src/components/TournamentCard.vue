d5b7af547aefbb53d85d1eb05b75443ca4f256f8
Trey Glover <trey.glover777@gmail.com>
2022-09-27 12:09:39 -0500

    tournament card base code started

---
+++ capstone/vue/src/components/TournamentCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class= 'card'>
+      <router-link v-bind:to="{name: 'tournament-details', params: {id: tournament.id}}">
+          <h2 class='tournament-name'>{{tournament.name}}</h2>
+          <img v-if='tournament.image' v-bind:src="tournament.image"/>
+          <ul class='tournament-minor-details'>
+              <li class='host-details'>
+                  <!-- <router-link to host page> -->
+                    Host: {{tournament.creator}}
+                  <!-- </router-link> -->
+              </li>
+              <li class='game'> Game: {{tournament.game}}</li>
+              <li class ='start-date'>Start-Date: {{tournament.startDate}}</li>
+          </ul>
+          <div class="tournament-status-container">
+              <h1 class = "tournament-status-display">{{tournament.status}}</h1>
+              <p class = "tournament-is-closed-text" v-if="tournament.status == 'closed'">Not accepting sign-ups</p>
+              <p class = "tournament-is-open-text" v-if="tournament.status == 'open'">Accepting sign-up</p>
+          </div>
+
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'tournament-card'
+
+}
+</script>
+
+<style>
+
+</style>
