021342938445b32ca400cf452e3ab07cec9d564b
Arthur Hutchinson <arthur.y.hutchinson@gmail.com>
2022-09-27 14:49:35 -0400

    Added a bunch of CSS stylizing to App.vue and it should affect all pages.

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -1,34 +1,83 @@
 <template>
   <div id="app">
+    <div id="navwithhr">
     <div id="nav">
       <!-- logo -->
-      <router-link v-bind:to="{ name: 'home' }">
-        <img id="logo" src="./assets/trnygg.png">
+      <div id="logo">
+      <router-link v-bind:to="{ name: 'home' }" >
+        <img src="./assets/trnygg.png">
       </router-link>
+      </div>
       <!--
         the v-if="$store.state.token" if it's ==, it's checking to see if the token is empty. Login and register will appear
         if it's != then that means token is not empty. They're logged in. Only logout will show.
       -->
       <div id="links">
-        <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''"> Login &nbsp;|&nbsp;</router-link>
+        <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''"> Login &nbsp;</router-link>
         <router-link v-bind:to="{ name: 'register' }" v-if="$store.state.token == ''">Register</router-link>
-        <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+        <router-link id="alogout" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
       </div>
     </div>
-    <hr>
+      <hr>
+    </div>
     <router-view />
   </div>
 </template>
 
 <style>
+
+  #navwithhr {
+    background-color: #232328;
+  }
+
   #nav {
     display: flex;
     justify-content: space-between;
+    align-items: flex-end;
   }
 
-  #logo {
+  #logo img {
     width: 250px;
   }
+
+  body {
+    background-color: #32323A;
+    color: white;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  hr {
+    height: 3px;
+    background-color: #825AB7;
+    border: none;
+  }
+
+  a {
+    color: white;
+  }
+
+  #links a:nth-child(1) {
+    color: #825AB7;
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 30px;
+  }
+
+  #links a:nth-child(2) {
+    color: #FC7900;
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 30px;
+  }
+
+  #links #alogout {
+    color: #FC7900;
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 30px;
+  }
+
+
 
 
 </style>
