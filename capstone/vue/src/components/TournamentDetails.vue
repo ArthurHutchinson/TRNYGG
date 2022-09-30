9b524206eab81d90cec086a9186c37a93cc2aa41
Tyler Knightly <TylerRKnightly@gmail.com>
2022-09-30 13:26:33 -0400

    Pushing Host Invite Working BM

--- capstone/vue/src/components/TournamentDetails.vue
+++ capstone/vue/src/components/TournamentDetails.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <div class="tournament-detail-box">
-            <router-link v-bind:to="{name: 'invite-player'}" v-bind:tournament="tournament">
+            <!-- <router-link v-bind:to="{name: 'invite-player'}" v-bind:tournament="tournament">
                 <button>Invite Player</button>
-            </router-link>
+            </router-link> -->
             <h1 class = "tournament-name">{{tournament.tournamentName}}</h1>
             <img class = "tournament-logo" v-if='tournament.imgUrl' v-bind:src="tournament.imgUrl"/>
             <h2 class = "tournament-game">{{tournament.game}}</h2>
@@ -15,6 +15,11 @@
             <!-- possibly include a router-link for the player id to player page -->
             </ul>
         </div>
+        <div>
+            
+            <b-button v-b-modal="'invite-player'"> Invite Player </b-button>
+            <b-modal id="invite-player"> <invite-form v-bind:tournament="tournament" /> </b-modal>
+        </div>
         <div class = "bracket-box">
             This is placeholder text
             <!-- we will put what we need to assemble the bracket here, for now there's placeholder text to be sure that it's at least, like, loading. -->
@@ -25,14 +30,20 @@
 
 <script>
     import TournamentService from '../services/TournamentService.js'
+    import InviteForm from '../components/InviteForm.vue'
 
 export default {
 
     name: 'tournament-details',
+    components: {
+        InviteForm,
+    },
+    
     data() {
         return {
             id: 0,
-            tournament: {}
+            tournament: {},
+            
         };
     },
 
