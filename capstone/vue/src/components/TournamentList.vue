73fef551e603942855c8588e743895c3658a9bc6
Arthur Hutchinson <arthur.y.hutchinson@gmail.com>
2022-10-03 18:12:05 -0400

    Fixed nav pages for trny

--- capstone/vue/src/components/TournamentList.vue
+++ capstone/vue/src/components/TournamentList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="tournament-list2">
+
     <div id="t-search-bar">
         <b-form id="t-search-bar-form">
           <b-form-input id="t-search-bar-form-input" size="sm" type="search" placeholder="Search Tournament Name, Game, or Organizer's Name Here" v-model="searchTerm"></b-form-input>
@@ -7,10 +8,37 @@
     </div>
     <!-- to tournament card below, add v-if search term is blank -->
     
-    <b-container id="t-list-b-container">
-    <b-row>
-      <tournament-card v-bind:tournament="tournament" v-for="tournament in filteredTournaments" v-bind:key="tournament.id" class ="t-card-in-list" />
+    <b-container id="t-list-b-container" class="overflow-auto" >
+
+    <b-pagination
+        id="t-pagination-card"
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        aria-controls="testpagination"
+        align="center"
+    />
+
+    <b-row id="testpagination">
+        
+      <tournament-card
+        class="t-card-in-list"
+        v-bind:tournament="tournament"
+        v-bind:key="tournament.id"
+        v-for="tournament in filteredTournaments.slice((currentPage-1) * perPage,(currentPage-1) * perPage + perPage)"
+      />
+
     </b-row>
+
+    <b-pagination
+        id="t-pagination-card"
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        aria-controls="testpagination"
+        align="center"
+    />
+
     </b-container>
 
   <!-- New tournament card element v-if search term is not blank that displays filtered list-->
@@ -29,6 +57,8 @@ export default {
         return {
             tournaments: [],
             searchTerm: "",
+            perPage: 12,
+            currentPage: 1,
         }
     },
     methods: {
@@ -49,8 +79,10 @@ export default {
                 tournament.organizerName.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
                 tournament.game.toLowerCase().includes(this.searchTerm.toLowerCase())
             })
-        
-    }
+        },
+        rows() {
+            return this.filteredTournaments.length;
+        }
 }
 }
 </script>
@@ -64,5 +96,9 @@ export default {
 
 #t-search-bar-form-input {
     text-align: center;
+}
+
+#t-pagination-card {
+    /* padding-top: 20px; */
 }
 </style>
